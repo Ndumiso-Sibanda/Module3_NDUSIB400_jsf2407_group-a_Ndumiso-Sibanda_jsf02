656be2f347973778d2040a69815ba0339d257425
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-07-30 10:23:15 +0200

    Imported all the pages and the routes

---
+++ e-commerce/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+ import Header from "./components/Header.svelte";
+ import { Router, Route } from "svelte-routing";
+ import Home from "./pages/Home.svelte";
+ import ProductDetailView from "./pages/ProductDetailView.svelte";
+</script>
+
+<main>
+<Header/>
+<Router>
+  <Route  path="/" component ={Home} />
+  <Route  path="products/:id" component ={ProductDetailView} />
+</Router>
+</main>
+
+<style>
+
+</style>
