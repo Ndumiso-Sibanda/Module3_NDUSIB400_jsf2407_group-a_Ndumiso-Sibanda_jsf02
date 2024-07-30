66adf4bcd45d9d178ee9ea5f69eb4acc6bc71890
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-07-30 16:07:25 +0200

    ADDED PRODUCT DETAIL

--- e-commerce/src/pages/ProductDetailView.svelte
+++ e-commerce/src/pages/ProductDetailView.svelte
@@ -1,11 +1,64 @@
 <script>
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+    import { Link } from "svelte-routing";
     
-</script>
-
-<main>
-    <h1>Product Detail page</h1>
-</main>
-
-<style>
+    export let id;
+    let product = writable(null);
+    let loading = writable(true);
+    let error = writable(null);
+  
+    const fetchProduct = async (id) => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+        if (!response.ok) {
+          throw new Error("Data fetching failed, please check your network connection.");
+        }
+        const data = await response.json();
+        product.set(data);
+      } catch (err) {
+        error.set(err.message);
+      } finally {
+        loading.set(false);
+      }
+    };
     
-</style>
+    onMount(() => {
+      fetchProduct(id);
+    });
+  </script>
+  
+  <main>
+    {#if $loading}
+      <p>Loading...</p>
+    {:else if $error}
+      <p>Error: {$error}</p>
+    {:else if $product}
+      <div class="product-detail">
+        <img src="{$product.image}" alt="{$product.title}" />
+        <h1>{$product.title}</h1>
+        <p>{$product.description}</p>
+        <p>$ {$product.price}</p>
+        <p>{$product.category}</p>
+        <p>Rating: {$product.rating.rate}</p>
+        <p>Number of Reviews: {$product.rating.count}</p>
+      </div>
+    {/if}
+    <Link to="/">Back to products</Link>
+  </main>
+  
+  <style>
+    .product-detail {
+      max-width: 250px;
+      margin: 0 auto;
+      text-align: center;
+      font-weight: bold;
+    }
+    .product-detail img {
+      max-width: 100%;
+      height: auto;
+      border-radius: 2px;
+      font-size: large;
+    }
+  </style>
+  
